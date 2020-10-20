2db67083a515211a33917636c8b09144395b309a
Eric Lam <ehylam.p@gmail.com>
2020-10-20 21:10:33 +1100

    three js fun

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,13 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	export let segment;
+
+
 </script>
 
 <style type="text/scss" global>
 	@import '../styles/main';
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+
 </style>
 
 <Nav {segment}/>
